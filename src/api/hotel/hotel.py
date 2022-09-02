d47499a5b948bc7ad7f0a30d0ef352e37cba45bd
AtharvaCM <necrons999@gmail.com>
2022-09-02 15:44:51 +0530

    refactored HotelList

--- src/api/hotel/hotel.py
+++ src/api/hotel/hotel.py
@@ -1,11 +1,16 @@
 # Hotel blueprint
 # All hotel related API will be maintained here
 
-import json
-from urllib import request
-from flask import Blueprint
-from flask import jsonify
+from flask import Blueprint, jsonify, request
 from flask_restful import Resource, Api, reqparse, abort
+
+# date helper
+from src.services.dateHepler import getCurrentDate, getNextDate
+
+# default values
+DEFAULT_LOCATION = 'Fallback_Pune'
+DEFAULT_CHECK_IN_DATE = getCurrentDate("%m/%d/%y")
+DEFAULT_CHECK_OUT_DATE = getNextDate("%m/%d/%y")
 
 hotel = Blueprint("hotel", __name__, url_prefix="/api/v1/hotels")
 api = Api(hotel)
@@ -15,12 +20,35 @@ parser = reqparse.RequestParser()
 # importing Model
 # from src.models.HotelModel.py import Hotel
 
+
+# class HotelList(Resource):
+#     def get(self):
+#         parser.add_argument('location', type=str, location='args')
+#         parser.add_argument(
+#             'check_in_date', type=str, location='args')
+#         parser.add_argument(
+#             'check_out_date', type=str, location='args')
+
+#         # getting query params
+#         location = request.args.get('location', DEFAULT_LOCATION, type=str)
+#         check_in_date = request.args.get(
+#             'check_in_date', DEFAULT_CHECK_IN_DATE, type=str)
+#         check_out_date = request.args.get(
+#             'check_in_date', DEFAULT_CHECK_OUT_DATE, type=str)
+
+#         return jsonify(parser.parse_args(location, check_in_date, check_out_date))
+
+
 class HotelList(Resource):
     def get(self):
-        location=parser.add_argument('location', type=str, location='args')
-        check_in_date=parser.add_argument('check_in_date', type=str, location='args')
-        check_out_date=parser.add_argument('check_out_date', type=str, location='args')
-        return jsonify(parser.parse_args(location, check_in_date, check_out_date))
-    
-api.add_resource(HotelList, '/')
+        # getting query params
+        location = request.args.get('location', DEFAULT_LOCATION, type=str)
+        check_in_date = request.args.get(
+            'check_in_date', DEFAULT_CHECK_IN_DATE, type=str)
+        check_out_date = request.args.get(
+            'check_in_date', DEFAULT_CHECK_OUT_DATE, type=str)
 
+        return jsonify(dict(data=[location, check_in_date, check_out_date]))
+
+
+api.add_resource(HotelList, '/')
