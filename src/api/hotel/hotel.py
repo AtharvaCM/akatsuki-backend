888f8ae3be6af727151aec8874848fc8b6949c94
Bunny0X1 <harsh.goplani@bluepineapple.io>
2022-09-07 11:24:19 +0530

    working hoteldetails api

--- src/api/hotel/hotel.py
+++ src/api/hotel/hotel.py
@@ -80,11 +80,11 @@ api.add_resource(HotelList, '/')
 
 class HotelDetails(Resource):
   @marshal_with(hotel_fields)
-  def get(self):
-        hotel_id = parser.add_argument('id', type=int)
-        hotel = Hotel.query.filter_by(id = hotel_id).first()  
+  def get(self,id):
+        #hotel_id = parser.add_argument('id', type=int)
+        hotel = Hotel.query.filter_by(id = id).first()  
         
         return hotel
     
-api.add_resource(HotelDetails, '/<int:Number>')
+api.add_resource(HotelDetails, '/<int:id>')
         
