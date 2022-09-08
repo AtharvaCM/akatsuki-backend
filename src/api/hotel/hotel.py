273b448e3406cdc8f2aac7326175e8d8dd2b93af
Bunny0X1 <harsh.goplani@bluepineapple.io>
2022-09-08 11:19:38 +0530

    room_type api added

--- src/api/hotel/hotel.py
+++ src/api/hotel/hotel.py
@@ -1,13 +1,17 @@
 # Hotel blueprint
 # All hotel related API will be maintained here
 
+from cgitb import text
+from datetime import date
 import json
+from os import O_RANDOM
+from tkinter.tix import COLUMN
 from flask import Blueprint, jsonify, request
 from flask_restful import Resource, Api, reqparse, abort, marshal_with, fields
 from src.database import db
 
 # importing Model
-from src.models import Hotel, Booking
+from src.models import Hotel, Booking, Room
 # date helper
 from src.services.dateHepler import getCurrentDate, getNextDate
 
@@ -43,9 +47,15 @@ hotel_fields = {
     "country": fields.String,
     "features": fields.List(fields.String),
     "room_images": fields.List(fields.String),
-    "hotel_dp": fields.String
+    "hotel_dp": fields.String,
+    "room_type":fields.String
 
 }
+
+hotel_room_type = {
+    "room_type": fields.List(fields.String)
+}
+
 
 # Get the list of hotels available for check_in_date and check_out_date at provided location
 
@@ -71,20 +81,42 @@ class HotelList(Resource):
         ).filter(Hotel.id.not_in(subquery)).order_by(Hotel.ratings.desc()).all()
         #hotels = db.session.query(Hotel, Booking).filter(db.and_(Hotel.city == 'Kovalam', Hotel.id.not_in(Booking.hotel_id))).all()
         
-
         return hotels
 
 api.add_resource(HotelList, '/')
 
-
-
 class HotelDetails(Resource):
-  @marshal_with(hotel_fields)
-  def get(self,id):
+    @marshal_with(hotel_fields)
+    def get(self,id):
         #hotel_id = parser.add_argument('id', type=int)
         hotel = Hotel.query.filter_by(id = id).first()  
         
         return hotel
     
 api.add_resource(HotelDetails, '/<int:id>')
+
+class RoomType(Resource):
+    @marshal_with(hotel_room_type)
+    def get(self, id):
+        Room_type = db.session.query(Hotel.room_type).filter_by(id = id).first()
         
+        return Room_type
+
+api.add_resource(RoomType, '/<int:id>/Room_type')
+
+'''
+hotel_post_args = reqparse.RequestParser()
+hotel_post_args.add_argument('booking_code', type=int, required=True)
+hotel_post_args.add_argument('booking_date', type=date, required=True)
+hotel_post_args.add_argument('total_amount', type=int, required=True)
+hotel_post_args.add_argument('payment_method', type=str, required=True)
+hotel_post_args.add_argument('dates', type=date, required=True)
+hotel_post_args.add_argument('travelers', type=int, required=True)
+hotel_post_args.add_argument('no_of_rooms', type=str, required=True)
+
+class BookingConfirm(Resource):
+    def post(self, HotelDetails):
+	    temp = {'Hotels': name}
+		.append(temp)
+		return temp
+'''
