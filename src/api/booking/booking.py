b964577a055cccdd78f1e9e03e57e33f06df03b9
Bunny0X1 <harsh.goplani@bluepineapple.io>
2022-09-13 13:26:09 +0530

    changes booking api

--- src/api/booking/booking.py
+++ src/api/booking/booking.py
@@ -1,6 +1,7 @@
 # booking blueprint
 # All booking related API will be maintained here
 
+import json
 from flask import Blueprint, jsonify
 from flask_restful import Api, Resource
 from datetime import date
@@ -46,24 +47,36 @@ hotel_post_args.add_argument('check_in_date', type=date, required=True)
 hotel_post_args.add_argument('check_out_date', type=date, required=True)
 hotel_post_args.add_argument('room_type', type=str, required=True)
 '''
+'''
+{
+"user_id" : 11,
+"booking_code": 101,
+"booking_date" : "21/2/2020",
+"total_amount" : 20000,
+"payment_method" : "CREDIT_CARD",
+"travelers" : 2,
+"no_of_rooms" : 1,
+"check_in_date" : "15/3/2020",
+"check_out_date" : "21/3/2020",
+"room_type" : "deluxe"
+}'''
 
 class BookingConfirm(Resource):
     def post(self):
         new_Booking = Booking(user_id = request.json.get('user_id', type=int), 
-                              booking_code =  request.json.get('booking_code', type= int)
-                              #booking_date =  request.args.get('booking_date'),
-                              #total_amount =  request.args.get('total_amount'),
-                              #payment_method = request.args.get('payment_method'),
-                              #travelers = request.args.get('travelers'),
-                              #no_of_rooms = request.args.get('no_of_rooms'),
-                              #check_in_date = request.args.get('check_in_date'),
-                              #check_out_date = request.args.get('check_out_date'),
-                              #room_type = request.args.get('room_type') 
-                              )
+                              booking_code =  request.json.get('booking_code', type= int),
+                              booking_date =  request.json.get('booking_date', type=date),
+                              total_amount =  request.json.get('total_amount', type=int),
+                              payment_method = request.json.get('payment_method', type=str),
+                              travelers = request.json.get('travelers', type= int),
+                              no_of_rooms = request.json.get('no_of_rooms', type= int),
+                              check_in_date = request.json.get('check_in_date', type=date),
+                              check_out_date = request.json.get('check_out_date', type=date),
+                              room_type = request.json.get('room_type', type=str))
         db.session.add(new_Booking)
         db.session.commit()
         show = requested_columns(request)
-        Booking_serialized = Booking.to_dict(show=show)
+        Booking_serialized = new_Booking.to_dict(show=show)
         return jsonify(dict(data=Booking_serialized))
     
 api = Api(Booking)
